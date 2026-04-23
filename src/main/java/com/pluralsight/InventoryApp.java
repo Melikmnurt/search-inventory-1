a21d2eb367ec33fb89bae41ef2385c5984c8ef8d
abdulmeliktuhaye <abdulmelikmohammednur123@gmail.com>
2026-04-23 13:33:47 -0500

    First Commit

--- src/main/java/com/pluralsight/InventoryApp.java
+++ src/main/java/com/pluralsight/InventoryApp.java
@@ -1,4 +1,34 @@
 package com.pluralsight;
 
+import java.util.ArrayList;         //I need it for Array List
+
 public class InventoryApp {
+
+    //This method creates and returns a list of products
+    public static ArrayList<Product> getInventory(){
+
+        //Create an empty list
+        ArrayList<Product> inventory = new ArrayList<>();
+
+        //adding products manually (hardcoded)
+        inventory.add(new Product(1234, "Hammer", 19.49));
+        inventory.add(new Product(2345, "Box of nails", 9.29));
+        inventory.add(new Product(4567, "2x4 wood", 9.99));
+        inventory.add(new Product(5678, "Screwdriver", 7.49));
+        inventory.add(new Product(6789, "Drill", 49.99));
+
+        //Return the list
+        return inventory;
+    }
+    //Main method (am starting the program here)
+    public static void main(String []args){
+
+        //Call the method and store the list
+        ArrayList<Product> inventory = getInventory();
+
+        //Loop through the list and print each product
+        for (Product p: inventory){
+            System.out.println(p);
+        }
+    }
 }
--- src/main/java/com/pluralsight/Product.java
+++ src/main/java/com/pluralsight/Product.java
@@ -1,4 +1,21 @@
 package com.pluralsight;
 
-public class Product {
+public class Product {           // This class represents one product
+
+    //Variables to store product info
+    private int id;
+    private String name;
+    private double price;
+
+    //Constructor (this runs when we create a new product)
+    public Product(int id, String name, double price) {
+        this.id = id;                  //Stores the id
+        this.name = name;             //Stores the name
+        this.price = price;          //Stores the price
+    }
+    //This method helps print the product
+    @Override
+    public String toString(){
+        return id + " | " + name + " | " + price;
+    }
 }
